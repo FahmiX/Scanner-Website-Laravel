ef41a250185113aa25b5fcb478a0e5dbbed7cf23
FahmiX <fahmiahmadfadilah7@gmail.com>
2023-03-20 15:13:01 +0700

    asdf

--- app/Http/Controllers/API/GudangAPI.php
+++ app/Http/Controllers/API/GudangAPI.php
@@ -2,4 +2,15 @@
 
 namespace App\Http\Controllers\API;
 
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class GudangAPI extends Controller
+{
+    // Cek Barang
+
+}
+
 ?>
